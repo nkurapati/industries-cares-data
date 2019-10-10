4dde4ad07fa6a30b6315b424ae142d0a0a2f0a44
Narendra Kurapati <nkurapati@salesforce.com>
2019-10-10 18:29:59 +0530

    Enable Cors

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
+var allowCrossDomainRequests = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomainRequests);
+
 // Serve static files
 app.use(express.static(__dirname + '/public'));
 
